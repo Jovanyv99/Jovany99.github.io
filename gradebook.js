1e248877228e21f4ddd6c91c317c944613668472
Jovanyv99 <jivelazquez1@my.waketech.edu>
2025-04-09 18:55:13 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,10 @@
+function fetchGradeData() {
+    console.log("Fetching ggrade data...");
+}
+
+function populateGradebook(data) {
+    console.log("Populating gradebook with data:", data);
+}
+
+const gradeData = fetchGradeData():
+populateGradebook(gradeData);
