fa9ff28620d512e4f765c82a1a63c50572d8c263
Jovanyv99 <jivelazquez1@my.waketech.edu>
2025-04-27 14:45:57 -0400

    Add files via upload

--- gradebook.js
+++ gradebook.js
@@ -1,10 +1,43 @@
 function fetchGradeData() {
-    console.log("Fetching ggrade data...");
+  console.log("Fetching grade data...");
+  let xhr = new XMLHttpRequest();
+  let apiRoute = "/api/grades";
+  xhr.onreadystatechange = function(){
+    let results;
+    if(xhr.readyState === xhr.DONE){
+      if(xhr.status !== 200){
+        console.error('Could not get grades. Status: ${xhr.status}');
+      } else {
+        populateGradebook(JSON.parse(xhr.responseText));
+      }
+    }
+  }.bind(this);
+  xhr.open("get", apiRoute, true);
+  xhr.send();
 }
 
 function populateGradebook(data) {
-    console.log("Populating gradebook with data:", data);
-}
+  console.log("Populating gradebook with data", data);
+  let tableEl = document.getElementById("gradebook");  // The table element in your HTML
+  data.forEach(function(assignment) {
+    let row = document.createElement("tr"); // Create new row
+    
+    let columns = {};
+    // First column: name
+    columns.name = document.createElement("td");
+    columns.name.appendChild(
+      document.createTextNode(assignment.last_name + ", " + assignment.first_name)
+    );
+    row.appendChild(columns.name);
 
-const gradeData = fetchGradeData():
-populateGradebook(gradeData);
+    // Second column: grade
+    columns.grade = document.createElement("td");
+    columns.grade.appendChild(
+      document.createTextNode(assignment.total_grade)
+    );
+    row.appendChild(columns.grade);
+
+    // Add row to table
+    tableEl.appendChild(row);
+  });
+}
